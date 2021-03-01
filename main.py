90bd270cef80808ee050de9e4bdceaecd0f344a4
Ayush Mall <mall.ayush0710@gmail.com>
2021-03-02 02:29:45 +0530

    CLI argument handling

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from sys import argv
 import os
 import json
 from pathlib import Path
+import argparse
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -21,19 +22,31 @@ def run(audio_file, model, normaliser):
     results = {"results":[]}
     for i in range(Y.shape[0]):
         tab = {}
-        if i%4==0:
-            print("\n" + "-"*50)
         tab["time"] = times[i]
         tab["tab"] = Y[i].tolist()
         tab["chord"] = get_chord(Y[i])
-        print(tab["time"])
-        print(Y[i], tab["chord"])
+        # print(tab["time"])
+        # print(Y[i], tab["chord"])
+        # if i%4==0:
+        #     print("\n" + "-"*50)
         results["results"].append(tab)
     return results
 
-audio_file = Path(argv[1])
-output_path = "output\\output.json"
+# Parsing Arguments
+parser = argparse.ArgumentParser(description='Automated Huitar Tablature Transcription')
+parser.add_argument('audio_path', help='Path of the guitar solo audio file', type=str)
+parser.add_argument('--output_path', help = 'Output directory (default: /output)', default="output", type=str)
+args = parser.parse_args()
+try: 
+    os.mkdir(args.output_path) 
+except FileExistsError as error: 
+    pass
+
+audio_file = Path(args.audio_path)
+output_path = args.output_path + "//output.json"
 results = run(audio_file, model = MODEL, normaliser = NORMALISER)
+
+# Writing output
 with open(output_path, "w") as outfile:  
         json.dump(results, outfile)
 print("Results saved at: ", output_path)
